204c4acc6c73e6473005473695a6729555740527
Nikmah Salsabila <nikmahsalsabila28@gmail.com>
2018-11-15 16:19:47 +0700

    add perencanaan model + user role model

---
+++ src/main/java/com/apap/farmasi/model/PerencanaanModel.java
@@ -0,0 +1,100 @@
+package com.apap.farmasi.model;
+
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "perencanaan")
+public class PerencanaanModel implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Column(name = "tanggal", nullable = false)
+	private Date tanggal;
+	
+	@NotNull
+	@Size(max = 255)
+	@Column(name = "status", nullable = false)
+	private String status;
+
+	@ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                CascadeType.PERSIST,
+                CascadeType.MERGE
+            })
+    @JoinTable(name = "perencanaan_medical_supplies",
+            joinColumns = { @JoinColumn(name = "id_medical_supplies") },
+            inverseJoinColumns = { @JoinColumn(name = "id_perencanaan") })
+    private List<MedicalSupplies> listMedicalSupplies;
+	
+	@NotNull
+	@Column(name = "jumlah", nullable = false)
+	private Integer jumlah;
+	
+	
+	public List<MedicalSupplies> getListMedicalSupplies() {
+		return listMedicalSupplies;
+	}
+
+	public void setListMedicalSupplies(List<MedicalSupplies> listMedicalSupplies) {
+		this.listMedicalSupplies = listMedicalSupplies;
+	}
+
+	public Integer getJumlah() {
+		return jumlah;
+	}
+
+	public void setJumlah(Integer jumlah) {
+		this.jumlah = jumlah;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getTanggal() {
+		return tanggal;
+	}
+
+	public void setTanggal(Date tanggal) {
+		this.tanggal = tanggal;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
