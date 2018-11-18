b4b6078b57eb9df785ab624b1603c8e79e9e18aa
Nikmah Salsabila <nikmahsalsabila28@gmail.com>
2018-11-18 13:58:58 +0700

    add user model

---
+++ src/main/java/com/apap/farmasi/model/UserModel.java
@@ -0,0 +1,67 @@
+package com.apap.farmasi.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "user")
+public class UserModel implements Serializable {
+	
+	@Id
+	@Size(max = 255)
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+	
+	@NotNull
+	@Size(max = 255)
+	@Column(name = "password", nullable = false, unique = true)
+	private String password;
+	
+	@NotNull
+	@Size(max = 255)
+	@Column(name = "role", nullable = false)
+	private String role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
