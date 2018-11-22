c4907d1a9ee94abada1f77890c93af0514d57ca0
aulia175 <aulia175@gmail.com>
2018-11-23 00:08:46 +0700

    fitur 12 tinggal masalah medical supply

--- src/main/java/com/apap/farmasi/controller/PermintaanRestController.java
+++ src/main/java/com/apap/farmasi/controller/PermintaanRestController.java
@@ -16,7 +16,8 @@ import java.util.List;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Time;
-
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.BindingResult;
@@ -45,49 +46,106 @@ public class PermintaanRestController {
 		BaseResponse<PermintaanModel> response = new BaseResponse<PermintaanModel>();
 
 		//date func
+		DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
 		Calendar today = Calendar.getInstance();
-		today.set(Calendar.HOUR_OF_DAY, 0); // same for minutes and seconds
+		df.setTimeZone(today.getTimeZone());
 		Date tanggalMasuk = new Date(today.getTimeInMillis()); 
+		String todayStr = df.format(today.getTime());
+		String[] todaySplit = todayStr.split("-");
+		String todayTahun = todaySplit[0];
+		String todayBulan = todaySplit[1];
+		String todayTanggal = todaySplit[2];
 		
 		//jadwal func
 		int jamMasuk = today.get(Calendar.HOUR);
+		
 		List<JadwalJagaModel> allJadwal = jadwalJagaDb.findAll();
-		System.out.println(allJadwal);
+//		System.out.println(allJadwal);
 		JadwalJagaModel jadwalDipakai = null;
 
 		//cari tanggal hari ini
 		for (JadwalJagaModel jadwalTemp : allJadwal) {
 			Date tempJadwalDate = jadwalTemp.getTanggal();
 			Time waktuJadwalMulai = jadwalTemp.getWaktuMulai();
+			
+			String waktuJadwalMulaiStr = waktuJadwalMulai.toString();
+			waktuJadwalMulaiStr = waktuJadwalMulaiStr.substring(0,2);
+//			System.out.println(waktuJadwalMulaiStr);			
+			int waktuJadwalMulaiInt = Integer.parseInt(waktuJadwalMulaiStr);
+
+			String jadwalDateStr = df.format(tempJadwalDate);
+			String[] splitJadwalDateStr = jadwalDateStr.split("-");
+			String jadwalDateTahun = splitJadwalDateStr[0];
+			String jadwalDateBulan = splitJadwalDateStr[1];
+			String jadwalDateTanggal = splitJadwalDateStr[2];
+			
+			
 			Calendar tempJadwalCal = Calendar.getInstance();
 			tempJadwalCal.setTime(tempJadwalDate);
 			Calendar tempJadwalMulai = Calendar.getInstance();
 			tempJadwalCal.setTime(waktuJadwalMulai);
-			System.out.println(today.get(Calendar.DATE));
-			System.out.println(today.get(Calendar.MONTH));
-			System.out.println(today.get(Calendar.YEAR));
+
+//			System.out.println(today);
+//			System.out.println(tempJadwalCal);
+//			System.out.println("-----------------------------------");
+//			System.out.println("jadwalid =" + jadwalTemp.getId());
+//			System.out.println(today.get(Calendar.DATE) +"=" + tempJadwalCal.get(Calendar.DATE));
+//			System.out.println(today.get(Calendar.MONTH) +"=" + tempJadwalCal.get(Calendar.MONTH));
+//			System.out.println(today.get(Calendar.YEAR) +"=" + tempJadwalCal.get(Calendar.YEAR));
+//			System.out.println("-----------------------------------");
+//			System.out.println(jadwalDateStr);
+//			System.out.println(tempJadwalDate);
+//			System.out.println(jadwalDateTanggal);
+//			System.out.println(jadwalDateBulan);
+//			System.out.println(jadwalDateTahun);
+//			System.out.println("-----------------------------------");
+//			System.out.println(todayTanggal + "=" +jadwalDateTanggal);
+//			System.out.println(todayBulan + "=" +jadwalDateBulan);
+//			System.out.println(todayTahun + "=" +jadwalDateTahun);
 			
-			
-			if (today.get(Calendar.DATE) == tempJadwalCal.get(Calendar.DATE) &&
-				today.get(Calendar.MONTH) == tempJadwalCal.get(Calendar.MONTH) &&
-				today.get(Calendar.YEAR) == tempJadwalCal.get(Calendar.YEAR)) {
-				
+			if (todayTanggal.equals(jadwalDateTanggal) &&
+				todayBulan.equals(jadwalDateBulan) &&
+				todayTahun.equals(jadwalDateTahun)){
+					
+//				System.out.println("MASOOOOOOOOOOOOK");
+//				System.out.println(today.get(Calendar.HOUR_OF_DAY));
+//				System.out.println(waktuJadwalMulaiInt);
 				if(today.get(Calendar.HOUR_OF_DAY)>=0 && 
 				   today.get(Calendar.HOUR_OF_DAY)<16 &&
-				   tempJadwalMulai.get(Calendar.HOUR_OF_DAY) == 8) {
+				    waktuJadwalMulaiInt == 8) {
+					System.out.println("masukakhir");
 					jadwalDipakai = jadwalTemp;
-					}
+				}
 				else if(today.get(Calendar.HOUR_OF_DAY)>=16 && 
-						today.get(Calendar.HOUR_OF_DAY)<=11 &&
-						tempJadwalMulai.get(Calendar.HOUR_OF_DAY) == 16) {
+					today.get(Calendar.HOUR_OF_DAY)<=24 &&
+					waktuJadwalMulaiInt == 16) {
 					jadwalDipakai = jadwalTemp;
-					
+					System.out.println("masukakhir");	
 				}
 			}
+			
+			
+			
+//			if (today.get(Calendar.DATE) == tempJadwalCal.get(Calendar.DATE) &&
+//				today.get(Calendar.MONTH) == tempJadwalCal.get(Calendar.MONTH) &&
+//				today.get(Calendar.YEAR) == tempJadwalCal.get(Calendar.YEAR)) {
+//				
+//				if(today.get(Calendar.HOUR_OF_DAY)>=0 && 
+//				   today.get(Calendar.HOUR_OF_DAY)<16 &&
+//				   tempJadwalMulai.get(Calendar.HOUR_OF_DAY) == 8) {
+//					jadwalDipakai = jadwalTemp;
+//					}
+//				else if(today.get(Calendar.HOUR_OF_DAY)>=16 && 
+//						today.get(Calendar.HOUR_OF_DAY)<=11 &&
+//						tempJadwalMulai.get(Calendar.HOUR_OF_DAY) == 16) {
+//					jadwalDipakai = jadwalTemp;
+//					
+//				}
+//			}
 		}
 		
-		System.out.println("masuk" + permintaan.getIdPasien());
-		permintaan.setIdJadwal(jadwalDipakai.getId());
+//		System.out.println("masuk" + jadwalDipakai.getId());
+		permintaan.setJadwalJaga(jadwalDipakai);
 		permintaan.setTanggal(tanggalMasuk);
 		permintaan.setStatusPermintaan(statusPermintaanDb.findById(1).get());
 		permintaanDb.save(permintaan);
