c2019e0713ce7b70b59bfaba9375732239297299
aulia175 <aulia175@gmail.com>
2018-12-06 15:16:34 +0700

    fitur dasar kelar semua, belum dirapihin

--- src/main/java/com/apap/farmasi/rest/ObatModel.java
+++ src/main/java/com/apap/farmasi/rest/ObatModel.java
@@ -1,5 +1,21 @@
 package com.apap.farmasi.rest;
 
 public class ObatModel {
-
+	String nama;
+	int jumlah;
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getJumlah() {
+		return jumlah;
+	}
+	public void setJumlah(int jumlah) {
+		this.jumlah = jumlah;
+	}
+	
+	
+	
 }
---
+++ src/main/java/com/apap/farmasi/rest/PasienModel.java
@@ -0,0 +1,16 @@
+package com.apap.farmasi.rest;
+
+public class PasienModel {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+
+}
