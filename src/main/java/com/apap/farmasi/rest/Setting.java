48837bc7a3e177513f5ccf12d6fc2a67f49ffd8d
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-12-10 23:33:51 +0700

    Update tampilan lagi

--- src/main/java/com/apap/farmasi/rest/Setting.java
+++ src/main/java/com/apap/farmasi/rest/Setting.java
@@ -4,5 +4,5 @@ public class Setting {
 	final public static String urlApt = "http://si-appointment.herokuapp.com/api";
 	final public static String urlMock = "https://74174c19-97ca-4974-9452-4abbf4da63b3.mock.pstmn.io";
 	final public static String urlRawatJalan = "http://rawat-jalan.herokuapp.com/api";
-	
+	final public static String urlLab = "http://silab10.herokuapp.com/lab/kebutuhan/perencanaan";
 }
